4fbb81ab1d7dcb6a11d64aa9708e392c13976473
vdeisner <privat_box@outlook.com>
2023-07-17 00:40:36 +0200

    added code to task_004

---
+++ task_004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    float result = i % 2;
+    if(result == 0)
+    {
+        Console.Write($"{i} ");
+    }
+
+}
